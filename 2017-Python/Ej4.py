ccce465e10c07fa97383e49d09d1f89dab162646
Paluchi <gonzandrelli@hotmail.com>
2017-04-20 11:54:56 -0300

    No testeado. terminado

--- 2017-Python/Ej4.py
+++ 2017-Python/Ej4.py
@@ -13,21 +13,20 @@ hora = None
 identificador = 0
 
 mi_empresa.set_nombre (str(input ("INGRESE EL NOMBRE DE LA EMPRESA: ")))
-print ("a")
 mi_empresa.set_telefono (input ("INGRESE EL NUMERO DE TELEFONO DE LA EMPRESA: "))
 
-input ("CUANTOS EMPLEADOS TIENE ESTA EMPRESA: ")
+cantidad_empleados = input ("CUANTOS EMPLEADOS TIENE ESTA EMPRESA: ")
 
-for i in range (cantidad_empledos + 1):
+for i in range (int(cantidad_empleados) + 1):
     mi_empleado.set_nombre (input ("INGRESE EL NOMBRE DEL EMPLEADO: "))
     mi_empleado.set_apellido (input("INGRESE EL APELLIDO DEL EMPLEADO: "))
     mi_empleado.set_telefono (input("INGRESE EL NUMERO DE TELEFONO DEL EMPLEADO: "))
     dia = input("DIA DE CUMPLEAÑOS (NUMERICAMENTE): ")
     mes = input("MES DE CUMPLEAÑOS (NUMERICAMENTE): ")
     ano = input("ANO DE CUMPLEAÑOS (NUMERICAMENTE): ")
-    mi_empleado.set_fecha_nacimiento (date (dia , mes , ano))
+    mi_empleado.set_fecha_nacimiento (int (dia) , int (mes) , int (ano))
     dias_labor = input ("INGRESE CANTIDAD DE DIAS A TRABAJAR POR SEMANA (MAX 5 , MIN 1): ")
-    for i in range (dias_labor + 1):
+    for i in range (int(dias_labor) + 1):
         mi_empleado.dias_de_labor (input ("INGRESE UN DIA DE LA SEMANA A TRABAJAR (0/1/2/3/4 SIENDO 0 LUNES Y 4 VIERNES): "))
     mi_empleado.set_idenficador (identificador)
     identificador += 1

81174bb3fb43826ccce14af3f96e2fb9d20d0933
alumno <gonzandrelli@hotmail.com>
2017-06-30 10:55:59 -0300

    falta obtener la info de la base de datos (pichi el que lee)

---
+++ 2017-Python/Trabajos/ProyectoModelo2017/clases/proyecto.py
@@ -0,0 +1,125 @@
+class Proyecto (object):
+    id_proyecto = None
+    titulo = None
+    descripcion = None
+    imagenes = []
+
+
+    def __init__(self , id , cursor):
+        self.id_proyecto = id
+        self.titulo = self.obtener_titulo(id , cursor)
+        self.descripcion = self.obtener_descripcion(id, cursor)
+        self.imagenes = self.obtener_imagenes(id, cursor)
+
+
+    def obtener_titulo (self , id , cursor):
+        cursor.execute("select entregar_titulo ("+str(id)+")")
+        self.titulo = cursor.fetchall()
+        return self.titulo
+
+    def obtener_descripcion (self , id , cursor):
+        cursor.execute("select entregar_descripcion ("+str(id)+")")
+        self.descripcion = cursor.fetchall()
+        return self.descripcion
+
+    def obtener_imagenes (self , id , cursor):
+        self.imagenes = Imagen()
+        self.imagenes.__init__(id , cursor)
+        return self.imagenes
+
+    def obtener_integrantes(self, cursor , id):
+        integrante = Integrante()
+        integrante.__init__(cursor, id)
+        return integrante
+
+
+
+class Integrante (object):
+    dni = None
+    nombre = None
+    apellido = None
+    curso = None
+    edad = None
+    imagen = None
+
+    def __init__(self, cursor, id):
+        self.dni = self.obtener_dni(cursor, id)
+        self.nombre = self.obtener_nombre(cursor, id)
+        self.apellido = self.obtner_apellido(cursor, id)
+        self.curso = self.obtener_curso(cursor, id)
+        self.edad = self.obtener_edad(cursor, id)
+        self.imagen = self.obtner_imagen(cursor, id)
+
+
+    def obtener_nombre(self, cursor, id):
+        cursor.execute("select integrante.nombre from integrante "
+                        "join integrnte_has_proyecto"
+                        "on integrante.dni = integrante_has_proyecto.integrante.dni"
+                        "where integrante_has_proyecto = ("+str(id)+")")
+        self.nombre = cursor.fetchall()
+
+    def obtener_dni(self, cursor, id):
+        cursor.execute("select integrante.dni from integrante "
+                        "join integrnte_has_proyecto"
+                        "on integrante.dni = integrante_has_proyecto.integrante.dni"
+                        "where integrante_has_proyecto = (" + str(id) + ")")
+        self.dni = cursor.fetchall()
+
+    def obtner_apellido(self, cursor, id):
+        cursor.execute("select integrante.apellido from integrante "
+                        "join integrnte_has_proyecto"
+                        "on integrante.dni = integrante_has_proyecto.integrante.dni"
+                        "where integrante_has_proyecto = (" + str(id) + ")")
+        self.apellido = cursor.fetchall()
+
+    def obtener_curso(self, cursor, id):
+        cursor.execute("select integrante.curso from integrante "
+                        "join integrnte_has_proyecto"
+                        "on integrante.dni = integrante_has_proyecto.integrante.dni"
+                        "where integrante_has_proyecto = (" + str(id) + ")")
+        self.curso = cursor.fetchall()
+
+    def obtener_edad(self, cursor, id):
+        cursor.execute("select integrante.edad from integrante "
+                       "join integrnte_has_proyecto"
+                       "on integrante.dni = integrante_has_proyecto.integrante.dni"
+                       "where integrante_has_proyecto = (" + str(id) + ")")
+        self.edad = cursor.fetchall()
+
+    def obtner_imagen(self, cursor, id):
+        cursor.execute("select integrante.imagen from integrante "
+                       "join integrnte_has_proyecto"
+                       "on integrante.dni = integrante_has_proyecto.integrante.dni"
+                       "where integrante_has_proyecto = (" + str(id) + ")")
+        self.imagen = cursor.fetchall()
+
+
+
+class Imagen (object):
+    id_imagen = None
+    imagen = None
+    descripcion = None
+
+    def __init__(self, id, cursor):
+        self.id_imagen = self.obtener_id_imagen(id , cursor)
+        self.imagen = self.obtener_imagenes(id , cursor)
+        self.descripcion = self.obtener_descipcion(id , cursor)
+
+
+    def obtener_imagenes (self , id , cursor):
+        cursor.execute("call dar_imagenes(" + str(id) + ")")
+        self.imagen = cursor.fetchall()
+        return self.imagen
+
+    def obtener_descipcion (self , id , cursor):
+        cursor.execute("call descripcion_imagenes(" + str(id) + ")")
+        self.descripcion = cursor.fetchall()
+        return self.descripcion
+
+    def obtener_id_imagen(self , id , cursor):
+        cursor.execute("select id_imagen from imagen where proyecto_id_grupo = (" + str(id) + ")")
+        self.id_imagen = cursor.fetchall()
+        return self.id_imagen
+
+
+
--- 2017-Python/Trabajos/ProyectoModelo2017/main.py
+++ 2017-Python/Trabajos/ProyectoModelo2017/main.py
@@ -1,34 +1,30 @@
 from flask import Flask
 from flask import render_template
+from clases.proyecto import *
 
 
 import pymysql
 
-db = pymysql.connect (host = '172.16.2.250' , user = "root" , password = "alumno" , db = "expo_modelo_2017_computacion" , autocommit = True)
-c= db.cursor()
+db = pymysql.connect (host = '172.16.2.250' , user = "root" , password = "alumno" ,
+                      db = "expo_modelo_2017_computacion" , autocommit = True)
+
+cursor = db.cursor()
 
 app = Flask(__name__)
+proyecto = Proyecto()
+
 
 @app.route('/')
-def index():
-    titulo = "Pantalla Principal"
-    descripcion = "ESTA DESCRIPCION ESTA RE LOCA"
-    imagenes = ["FOTOS.PNG", "FOTOS.PNG"]#lista de rutas de fotos del proyecto
+def index(cursor):
+
     guia = "COLOCA UNA PIEZA PARA SABER MAS INFORMACION SOBRE ESE PROYECTO"
     return render_template('Index.html', titulo = titulo, descripcion = descripcion, imagenes = imagenes, guia = guia)
+
 
 @app.route('/Proyecto')
 def proyecto (titulo, descripcion, imagenes, estadisticas):
     return render_template('Proyecto.html', titulo = titulo,  descripcion = descripcion, imagenes = imagenes, estadisticas = estadisticas)
 
-
-def seleccionarProyecto(codigo):
-    titulo = c.execute("select entregar_titulo(codigo)")
-    descripcion = c.execute("select entregar_descripcion(codigo)")
-    imagenes = c.execute("select dar_imagenes(codigo)")
-    imagenes = imagenes.split("º")
-    estadisticas = ""
-    proyecto(titulo, descripcion, imagenes, estadisticas)
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5000)

4ddda95bb11432797f8755b0b8f856aebc2c10f8
alumno <alumno>
2017-09-28 11:58:59 -0300

    gvjhgf

--- 2017-Python/Trabajos/ProyectoModelo2017/main.py
+++ 2017-Python/Trabajos/ProyectoModelo2017/main.py
@@ -19,12 +19,11 @@ def ObtenerProyecto(id, db):
     titulo = proyecto.obtener_titulo(id, db)
     descripcion = proyecto.obtener_descripcion(id, db)
     imagenes = proyecto.obtener_imagenes(id, db)
-    descripcionImagen = proyecto.obtener_descripcionImagen(id, db)
-    nombres, nombre1, nombre2, nombre3, nombre4, nombre5, nombre6 = proyecto.obtener_integrantes(db, id)
     curso = proyecto.obtener_curso(id, db)
-    return titulo, descripcion, imagenes, descripcionImagen, nombres, nombre1, nombre2, nombre3, nombre4, nombre5, nombre6, curso
+    return titulo, descripcion, imagenes, curso
 
-
+def ObtenerNombres(id, db):
+    nombres, nombre1, nombre2, nombre3, nombre4, nombre5, nombre6 = proyecto.obtener_integrantes(db, id)
 @app.route('/')
 def index():
     return render_template('Index.html', static = HTTP_STATIC)
@@ -32,9 +31,9 @@ def index():
 @app.route('/Proyecto')
 def proyecto ():
     id = 8
-    titulo, descripcion, imagenes, descripcionImagen, nombres, nombre1, nombre2, nombre3, nombre4, nombre5, nombre6, curso  = ObtenerProyecto(id, db)
-    return render_template('Proyecto.html', static = HTTP_STATIC, curso = curso, titulo = titulo, descripcion = descripcion, imagenes = imagenes, nombres = nombres, nombre1 = nombre1, nombre2 = nombre2, nombre3 = nombre3, nombre4 = nombre4, nombre5 = nombre5, nombre6 = nombre6)
-
+    titulo, descripcion, imagenes, descripcionImagen, curso  = ObtenerProyecto(id, db)
+    listaNombre = obtenerNombres(id, db)
+    return render_template('Proyecto.html', static = HTTP_STATIC, curso = curso, titulo = titulo, descripcion = descripcion, imagenes = imagenes, listaNombre = listaNombre)
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5000)

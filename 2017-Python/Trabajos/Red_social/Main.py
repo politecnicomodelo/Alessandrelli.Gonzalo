ca1df457e7923bab541e5740c65e05656d06eae4
alumno <ssdf>
2017-10-31 17:41:46 -0300

    todo

--- 2017-Python/Trabajos/Red_social/Main.py
+++ 2017-Python/Trabajos/Red_social/Main.py
@@ -17,7 +17,8 @@ HTTP_STATIC="http://127.0.0.1/"
 app = Flask(__name__)
 @app.route('/')
 def index():
-  return render_template('index.html', static = HTTP_STATIC)
-
+    lista = ["amigo1","amigo2","amigo3","amigo4","amigo5"]
+    return render_template('index.html', static = HTTP_STATIC,  lista = lista)
+    id = request.args.get('id')
 if __name__ == '__main__':
     app.run(debug = True, port = 5000)

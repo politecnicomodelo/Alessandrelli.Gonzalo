c572c78e59fb6e70fb76563f26309e6850cb5f28
alessa <you@example.com>
2018-06-19 17:39:23 -0300

    gggg

--- 2017-Python/TPMongo/main.py
+++ 2017-Python/TPMongo/main.py
@@ -4,15 +4,31 @@ import csv
 
 x=[]
 y=[]
+barrio=[]
 capacidad=[]
 nombre=[]
-with open('locales.csv') as mongo:
-   reader = csv.reader(mongo)
-   for row in reader:
-       x.append(row[0])
-       y.append(row[1])
-       capacidad.append(row[8])
-       nombre.append(row[4])
+c=0
+h=0
+
+with open('locales-bailables.csv') as mongo:
+    reader = csv.reader(mongo, delimiter=";")
+    for row in reader:
+        if c==0:
+            c=c+1
+            pass
+        else:
+            x.append(row[0])
+            y.append(row[1])
+            barrio.append(row[24])
+            if row[7] == ' ':
+                capacidad.append(1)
+            else:
+                capacidad.append(int(row[7]))
+            nombre.append(row[4])
+
+
+
+
 
 app = Flask(__name__)
 
@@ -25,7 +41,41 @@ def index():
 
 @app.route('/barras')
 def barras():
-   return render_template('barras.html', capacidad=capacidad, nombre=nombre)
+    chico = 0
+    med = 0
+    gran = 0
+
+    for item in capacidad:
+        if item < 1000:
+            chico += 1
+
+        elif (item >= 1000) and (item < 2000):
+            med += 1
+
+        else:
+            gran += 1
+
+    nombre = "Cuadro Mongo"
+
+    return render_template('barras.html', capacidad=capacidad, nombre=nombre, gran=gran, med=med, chico=chico)
+
+@app.route('/torta')
+def torta():
+    chico = 0
+    med = 0
+    gran = 0
+
+    for item in capacidad:
+        if item < 1000:
+            chico += 1
+
+        elif (item >= 1000) and (item < 2000):
+            med += 1
+
+        else:
+            gran += 1
+
+    return render_template('torta.html', capacidad=capacidad, gran=gran, med=med, chico=chico)
 
 @app.route('/maps')
 def maps():
